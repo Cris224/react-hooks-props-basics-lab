ffdf15ee7e3bd79ea7be53c1f1057d5833e04154
Cristofer <cristoferlorenzana@gmail.com>
2021-11-03 14:23:57 -0500

    commit tree a81231b73746bb28b9bbe48755105c16cba852b4

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import user from "../data/user";
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{user.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <h3>Links</h3>
+      <a href={user.links.github}>{user.links.github}</a>
+      <a href={user.links.linkedin}>{user.links.linkedin}</a>
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
+      <Home username={user.name} city={user.city} color={user.color} />
       <About />
     </div>
   );
